0bdea371e8d26222389dc90c067400f278c7ee96
Bruno Serafim <bcserafim@gmail.com>
2024-02-22 14:25:32 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -14,6 +14,8 @@ namespace SalesWebMvc.Models
 
         public Department Department { get; set; }
 
+        public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
 
